fcc7036067ceb711110f30ec321192c02be25bc7
krawnick <kraw.nick@gmail.com>
2023-07-23 15:59:39 +0500

    lesson10 (test)

--- test/index.ts
+++ test/index.ts
@@ -1,34 +1,19 @@
-// const isBirthdayData: boolean = true
-// let ageData: number = 28
-// const userNameData: string = 'Nikita'
+const isBirthdayData: boolean = false
+let ageData: number = 28
+const userNameData: string = 'Nikita'
 
-const userData =
-  '{"isBirthdayData": true, "ageData": 28, "userNameData": "Nikita"}'
-
-const userObj: {
-  isBirthdayData: boolean
-  userNameData: string
-  ageData: number
-} = JSON.parse(userData)
-
-console.log(userObj)
-
-// function logBrtMsg(isBirthday: boolean, userName: string, age: number): string {
-//   return isBirthday
-//     ? `Congrats ${userName.toUpperCase()}, age: ${age + 1}`
-//     : 'Error!'
-// }
-
-const logBrtMsg = (
-  isBirthday: boolean,
-  userName: string,
-  age: number
-): string => {
-  return isBirthday
-    ? `Congrats ${userName.toUpperCase()}, age: ${age + 1}`
-    : 'Error!'
+const createError = (message: string): never => {
+  console.error(message)
+  throw new Error(message)
 }
 
-console.log(
-  logBrtMsg(userObj.isBirthdayData, userObj.userNameData, userObj.ageData)
-)
+function logBrtMsg(isBirthday: boolean, userName: string, age: number): string {
+  if (isBirthday === true) {
+    return `Congrats ${userName.toUpperCase()}, age: ${age + 1}`
+  } else if (isBirthday === false) {
+    return 'Too bad'
+  }
+  return createError('Error!')
+}
+
+logBrtMsg(isBirthdayData, userNameData, ageData)
