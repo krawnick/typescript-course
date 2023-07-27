ff0527692f66184af0566d97b67d1df3ab744259
krawnick <kraw.nick@gmail.com>
2023-07-27 21:56:23 +0500

    lesson18 (test)

--- test/index.ts
+++ test/index.ts
@@ -1,9 +1,68 @@
 const message: string | number = 5
 const messages: string[] | number[] = ['a', 'b']
 
-function printMsg(msg: string | number): void {
-  console.log(msg)
+// function printMsg(msg: string | number): void {
+//   if (typeof msg === 'string') {
+//     console.log(msg.toLowerCase())
+//   } else {
+//     console.log(msg.toExponential())
+//   }
+// }
+
+// printMsg(4)
+// printMsg('a')
+
+// function printMsg(msg: string | number | boolean): void {
+//     if (typeof msg === 'string' || typeof msg === 'number') {
+//       console.log(msg.toString())
+//     } else {
+//       console.log(msg)
+//     }
+//     console.log(msg)
+//   }
+
+//   printMsg(4)
+//   printMsg('a')
+
+// function printMsg(msg: string[] | number | boolean): void {
+//   if (Array.isArray(msg)) {
+//     msg.forEach((m) => console.log(m))
+//   } else if (typeof msg === 'number') {
+//     console.log(msg.toFixed())
+//   } else {
+//     console.log(msg)
+//   }
+// }
+
+// printMsg(4)
+// printMsg(['a'])
+
+const printReadings = (a: number | string, b: number | boolean): void => {
+  if (a === b) {
+    console.log(a, b)
+  }
 }
 
-printMsg(4)
-printMsg('a')
+const printReadings2 = (a: number[] | string): void => {
+  console.log(a.slice(0, 3))
+}
+
+const checkReadings = (
+  readings: { system: number } | { user: number }
+): void => {
+  if ('system' in readings) {
+    console.log(readings.system)
+  } else {
+    console.log(readings.user)
+  }
+}
+
+function logValue(x: string | Date) {
+  if (x instanceof Date) {
+    console.log(x.getFullYear())
+  } else {
+    console.log(x)
+  }
+}
+
+console.log(logValue(new Date()))
