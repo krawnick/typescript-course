f1afe1611eeb63bf166118801167e85ab01da0f5
krawnick <kraw.nick@gmail.com>
2023-07-30 22:21:34 +0500

    lesson20 (test)

--- test/index.ts
+++ test/index.ts
@@ -3,27 +3,31 @@ let msg: 'Hello' = 'Hello'
 const port3000: number = 3000
 const port3001: number = 3001
 
-function startServer(protocol: 'http' | 'https', port: 3000 | 3001 ): 'Server started' {
-
+function startServer(
+  protocol: 'http' | 'https',
+  port: 3000 | 3001
+): 'Server started' {
   if (port === port3000 || port3001) {
-
-  console.log(`Server started on ${protocol}://server:${port}`)
+    console.log(`Server started on ${protocol}://server:${port}`)
   } else {
     console.error('Invalid port')
   }
-  
-  
+
   return 'Server started'
 }
 
 startServer('https', 3001)
 
-function createAnimation(id: string | number, 
-animName: string, 
-timingFunc: 'ease' | 'ease-out' | 'ease-in' = 'ease',
-duration: number,
-iterCount: number | 'infinity'): void {
+type AnimationTimingFunc = 'ease' | 'ease-out' | 'ease-in'
+type AnimationId = string | number
 
+function createAnimation(
+  id: AnimationId,
+  animName: string,
+  timingFunc: AnimationTimingFunc = 'ease',
+  duration: number,
+  iterCount: number | 'infinity'
+): void {
   // const elem = document.querySelector(`#${id}`) as HTMLElement
 
   // if (elem) {
