1d66a5889d8d83e51399d74bb1461673ed840a0e
krawnick <kraw.nick@gmail.com>
2023-08-01 22:24:59 +0500

    lesson24 (test)

---
+++ test/interface.ts
@@ -0,0 +1,29 @@
+interface IConfig {
+  protocol: 'http' | 'https'
+  port: 3000 | 3001
+}
+// Возможно расширение интрефейса
+interface IConfig {
+  date: Date
+}
+
+interface IRole {
+  role: string
+}
+
+interface IConfigWithRole extends IConfig, IRole {}
+
+const serverIConfig: IConfigWithRole = {
+  protocol: 'https',
+  port: 3001,
+  role: 'admin',
+  date: new Date(),
+}
+
+const startIServer = (
+  protocol: 'http' | 'https',
+  port: 3000 | 3001
+): 'Server started' => {
+  console.log(`Server started on ${protocol}://server:${port}`)
+  return 'Server started'
+}
