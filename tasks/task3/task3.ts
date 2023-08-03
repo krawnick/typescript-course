71192d7206d957b2e94ceee09c9e47c2ca00f4c9
krawnick <kraw.nick@gmail.com>
2023-08-03 19:27:02 +0500

    task3 ready

--- tasks/task3/task3.ts
+++ tasks/task3/task3.ts
@@ -1,38 +1,41 @@
 // Структура данных скалада с одеждой
 
-// ClothesWarehouse {
-//   jackets: 'empty' | number
-//   hats: 'empty' | number
-//   socks: 'empty' | number
-//   pants: 'empty' | number
-// }
+interface ClothesWarehouse {
+  jackets: 'empty' | number
+  hats: 'empty' | number
+  socks: 'empty' | number
+  pants: 'empty' | number
+}
 
 // Структура данных склада с канцтоварами
 
-// StationeryWarehouse {
-//   scissors: 'empty' | number
-//   paper: 'empty' | boolean
-// }
+interface StationeryWarehouse {
+  scissors: 'empty' | number
+  paper: 'empty' | boolean
+}
 
 // Структура данных склада с бытовой техникой
 
-// AppliancesWarehouse {
-//   dishwasers: 'empty' | number
-//   cookers: 'empty' | number
-//   mixers:  'empty' | number
-// }
+interface AppliancesWarehouse {
+  dishwashers: 'empty' | number
+  cookers: 'empty' | number
+  mixers: 'empty' | number
+}
 
 // Общая стурктура данных, наследует все данные из трех выше
 // + Добавляет свои
 
-// TotalWarehouse {
-//   deficit: boolean
-//   date: Date;
-// }
+interface TotalWarehouse
+  extends ClothesWarehouse,
+    StationeryWarehouse,
+    AppliancesWarehouse {
+  deficit: boolean
+  date: Date
+}
 
 // Главный объект со всеми данными, должен подходить под формат TotalWarehouse
 
-const totalData = {
+const totalData: TotalWarehouse = {
   jackets: 5,
   hats: 'empty',
   socks: 'empty',
@@ -42,10 +45,30 @@ const totalData = {
   dishwashers: 3,
   cookers: 'empty',
   mixers: 14,
+  deficit: true,
+  date: new Date(),
 }
 
-function printReport(data) {
-  return `We need this items: ${'...'}`
-  // или
-  return 'Everything fine'
+function printReport(data: TotalWarehouse): string {
+  // const need = Object.entries(data)
+  // .filter(product => product[1] === 'empty')
+  // .reduce((acc, product) => {},)
+  const need = Object.entries(data)
+    .filter((product) => product[1] === 'empty')
+    .reduce((acc, product) => `${acc} ${product[0]},`, '')
+
+  return need.length
+    ? `We need this items: ${need.slice(0, -1)}`
+    : 'Everything fine'
 }
+
+console.log(printReport(totalData))
+
+// Реализуйте функцию, которая принимает в себя главный объект totalData нужного формата
+// и возвращает всегда строку
+// Функция должна отфильтровать данные из объекта и оставить только те названия товаров, у которых значение "empty"
+// и поместить их в эту строку. Если таких товаров нет - возвращается другая строка (см ниже)
+
+// С данным объектом totalData строка будет выглядеть:
+// "We need this items: hats, socks, cookers"
+// Товары через запятую, в конце её не должно быть. Пробел после двоеточия, в конце строки его нет.
