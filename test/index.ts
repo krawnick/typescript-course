dc0de1b29ef13825e9fdab6b8e48ca61d948a513
krawnick <kraw.nick@gmail.com>
2023-08-04 14:05:44 +0500

    lesson26 (test)

--- test/index.ts
+++ test/index.ts
@@ -7,8 +7,16 @@ interface UserData {
   userNameData: string
 }
 
+const arr = ['sss', 5, true]
+
 const userData =
   '{"isBirthdayData": true, "ageData": 28, "userNameData": "Nikita"}'
 
 const userObj = JSON.parse(userData)
 console.log(userObj)
+
+let isOkay = true
+let movement: boolean | string = false
+if (isOkay) {
+  movement = 'moving'
+}
