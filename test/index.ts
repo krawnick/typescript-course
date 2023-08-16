f2025e3b89d49d2fa9361964307ad243afb653de
krawnick <kraw.nick@gmail.com>
2023-08-16 15:24:21 +0500

    lesson37 (test)

--- test/index.ts
+++ test/index.ts
@@ -23,6 +23,7 @@ function isNumber(n: unknown): n is number {
 }
 
 interface Car {
+  name: 'car'
   engine: string
   wheels: {
     number: number
@@ -31,28 +32,66 @@ interface Car {
 }
 
 interface Ship {
+  name: 'ship'
   engine: string
   sail: string
 }
 
-function repairVechile(vehicle: Car | Ship) {
-  if (isCar(vehicle)) {
-    vehicle.wheels
-  } else if (isShip(vehicle)) {
-    vehicle.sail
-  } else {
-    vehicle
-  }
+interface Airplane {
+  name: 'airplane'
+  engine: string
+  wings: string
 }
+
+interface SuperAirplane {
+  name: 'smth'
+  engine: string
+  wings: string
+}
+
+type Vehicle = Car | Ship | Airplane | SuperAirplane
 
 // function isCar(car: Car | Ship): car is Car {
 //   return 'wheels' in car
 // }
 
-function isCar(car: Car | Ship): car is Car {
+function isCar(car: Vehicle): car is Car {
   return (car as Car).wheels.number !== undefined
 }
 
-function isShip(ship: Car | Ship): ship is Ship {
+function isShip(ship: Vehicle): ship is Ship {
   return 'sail' in ship
+}
+
+function repairVechile(vehicle: Vehicle) {
+  // if (isCar(vehicle)) {
+  //   vehicle.wheels
+  // } else if (isShip(vehicle)) {
+  //   vehicle.sail
+  // } else {
+  //   vehicle.wings
+  // }
+
+  switch (vehicle.name) {
+    case 'car': {
+      console.log(vehicle.wheels)
+      break
+    }
+    case 'ship': {
+      console.log(vehicle.sail)
+      break
+    }
+    case 'airplane': {
+      console.log(vehicle.wings)
+      break
+    }
+    case 'smth': {
+      console.log(vehicle.wings)
+      break
+    }
+    default: {
+      const smth: never = vehicle
+      console.log('oups')
+    }
+  }
 }
