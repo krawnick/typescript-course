f6fbc1b1e29c787a33a8c59382d15b21ce3c69cb
krawnick <kraw.nick@gmail.com>
2023-08-18 14:35:19 +0500

    ready task5

--- tasks/task5/index.ts
+++ tasks/task5/index.ts
@@ -28,11 +28,61 @@
 //     }
 // }
 
-function checkAnimalData(animal) {
-  if ('available') {
-    // Заменить условие!
+type Animal = 'cat' | 'dog' | 'bird'
+
+enum AnimalStatus {
+  Available = 'available',
+  NotAvailable = 'notAvailable',
+}
+
+interface AnimalAvailableData {
+  animal: Animal
+  breed: string
+  sterilized?: boolean
+  location: string
+  age?: number
+}
+
+interface AnimalNotAvailableData {
+  message: string
+  nextUpdateIn: Date
+}
+
+interface AnimalAvailableResponse {
+  status: AnimalStatus.Available
+  data: AnimalAvailableData
+}
+
+interface AnimalNotAvailableResponse {
+  status: AnimalStatus.NotAvailable
+  data: AnimalNotAvailableData
+}
+
+type Res = AnimalAvailableResponse | AnimalNotAvailableResponse
+
+function isAvailable(res: Res): res is AnimalAvailableResponse {
+  if (res.status === AnimalStatus.Available) {
+    return true
+  }
+  return false
+}
+
+function checkAnimalData(animal: Res): AnimalAvailableData | string {
+  if (isAvailable(animal)) {
     return animal.data
   } else {
     return `${animal.data}, you can try in ${animal.data.nextUpdateIn}`
   }
 }
+
+const reuslt = checkAnimalData({
+  status: AnimalStatus.Available,
+  data: {
+    animal: 'cat',
+    breed: 'snow-shu',
+    sterilized: true,
+    location: 'V-Pyshma',
+  },
+})
+
+console.log(reuslt)
