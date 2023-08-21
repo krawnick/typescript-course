7befb86f9780521a29a87f8fe2080da03691ecc5
krawnick <kraw.nick@gmail.com>
2023-08-21 19:25:09 +0500

    task6 ready js

---
+++ tasks/task6/index.js
@@ -0,0 +1,60 @@
+const forms = document.querySelectorAll('form')
+const email = document.querySelector('#email')
+const title = document.querySelector('#title')
+const text = document.querySelector('#text')
+const checkbox = document.querySelector('#checkbox')
+
+const formData = {
+  email: '',
+  title: '',
+  text: '',
+  checkbox: false,
+}
+
+console.log(Object.values(formData).every((el) => el))
+
+forms.forEach((form) => {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    formData.email = email.value
+    formData.title = title.value
+    formData.text = text.value
+    formData.checkbox = checkbox.checked
+
+    console.log(formData)
+    if (validateFormData(formData)) {
+      checkFormData(formData)
+    }
+  })
+})
+
+// Последовательность действий:
+// 1) Происходит submit любой из форм
+// 2) Все данные из 4х полей со страницы переходят в свойства объекта formData
+// 3) Запускается функция validateFormData с этим объектом, возвращает true/false
+// 4) Если на предыдущем этапе true, то запускается функция checkFormData с этим объектом
+
+function validateFormData(data) {
+  // Если каждое из свойств объекта data правдиво...
+  if (Object.values(data).every((value) => value)) {
+    console.log('All fields are completed!')
+    return true
+  } else {
+    console.log('Please, complete all fields')
+    return false
+  }
+}
+
+function checkFormData(data) {
+  const { email } = data
+  const emails = ['example@gmail.com', 'example@ex.com', 'admin@gmail.com']
+  console.log(email)
+
+  // Если email совпадает хотя бы с одним из массива
+  if (emails.includes(email)) {
+    console.log('This email is already exist')
+  } else {
+    console.log('Posting data...')
+  }
+}
