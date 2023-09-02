1b5609ad334a1c7ef874e815cc233c4a66c60a16
krawnick <kraw.nick@gmail.com>
2023-09-02 18:30:22 +0500

    task9-2 ready

---
+++ tasks/task9/task9-2.ts
@@ -0,0 +1,16 @@
+interface IForm {
+  login: string
+  password: string
+}
+
+type Valid<T> = {
+  [P in keyof T]: { isValid: true } | { isValid: false; errorMsg: string }
+}
+// Необходимо типизировать объект валидации
+// Учтите, что данные в форме могут расширяться и эти поля
+// должны появиться и в объекте валидации
+
+const validationData = {
+  login: { isValid: false, errorMsg: 'At least 3 characters' },
+  password: { isValid: true },
+}
