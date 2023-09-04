f845a51b18a87af5f163e1d02c186b03e4f27637
Kraw Nick <106428254+krawnick@users.noreply.github.com>
2023-09-04 18:48:41 +0500

    lesson61 ready

--- test/index.ts
+++ test/index.ts
@@ -1,53 +1,21 @@
-interface Currencies {
-  usa: 'usd'
-  china: 'cby'
-  ukraine: 'uah'
-  kz: 'tenge'
+function calculate(a: number, b: number): number {
+
+	return a * b
 }
 
-type CurrWithoutUsaAndUk = Omit<Currencies, 'usa' | 'ukraine'> // исключение
+type CalculateRT = ReturnType<typeof calculate>
 
-const currUsaAndUkraine: CurrWithoutUsaAndUk = {
-  china: 'cby',
-  kz: 'tenge',
+let anotherResult: CalculateRT = 5
+
+type calculatePT = Parameters<typeof calculate>
+type calculatePT0 = Parameters<typeof calculate>[0]
+
+type PT1 = Parameters<(a: number) => number>
+type PT2 = Parameters<<T>(arg: T) => T>
+
+class Example {
+	constructor(a: number)
+	{}
 }
 
-type CurrUsaAndUkraine = Pick<Currencies, 'usa' | 'ukraine'> // фильтрация по свойству
-
-const currUsaAndUkraine2: CurrUsaAndUkraine = {
-  usa: 'usd',
-  ukraine: 'uah',
-}
-
-type CountriesWithoutUsa = Exclude<keyof Currencies, 'usa'>
-
-type CreateCustomCurr<T> = {
-  [P in keyof T as `custom${Capitalize<string & P>}`]: string
-}
-
-type PlayersNames = 'alex' | 'john'
-type CustomCurrencies = CreateCustomCurr<Currencies>
-type GameDataCurr = Record<PlayersNames, CustomCurrencies>
-
-const gameData: GameDataCurr = {
-  alex: {
-    customChina: 'qwerty',
-    customKz: '213',
-    customUkraine: '12ed',
-    customUsa: '213dsad',
-  },
-  john: {
-    customChina: 'qwerasdty',
-    customKz: '2132qe',
-    customUkraine: '12edasd',
-    customUsa: '213dsasdad',
-  },
-}
-
-type FadeType = Exclude<MyAnimation, 'swipe'> // удаление из union type
-type SwipeType = Extract<MyAnimation | Direction, 'swipe'> // выбор подходящего типа
-
-type MyAnimation = 'fade' | 'swipe'
-type Direction = 'in' | 'out'
-
-type MyNewAnimation = `${MyAnimation}${Capitalize<Direction>}`
+type T0 = ConstructorParameters<typeof Example>
