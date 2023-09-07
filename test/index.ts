66572a1b8385dc96b3b39af60fca6690e6f6acc6
krawnick <kraw.nick@gmail.com>
2023-09-07 20:12:40 +0500

    lesson65 test

--- test/index.ts
+++ test/index.ts
@@ -1,3 +1,22 @@
+// class Box {
+// 	// width!: number; || ts config: strictPropertyInitialization: false
+// 	width: number;
+// 	height: number;
+// 	volume: string;
+
+// 	constructor(volume: string)
+// 	constructor(width: number)
+// 	constructor(widthOrVolume: number | string) {
+// 		if (typeof widthOrVolume === 'number') {
+// 			this.width = widthOrVolume
+// 		} else {
+// 			this.volume = widthOrVolume
+// 		}
+
+// 		this.height = 500
+// 	}
+// }
+
 class Box {
   // width!: number; || ts config: strictPropertyInitialization: false
   width: number
@@ -14,7 +33,7 @@ const firstBox = new Box(200)
 console.log(firstBox)
 
 class User {
-  name!: string
+  name: string
 }
 
 const ivan = new User()
