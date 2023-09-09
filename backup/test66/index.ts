ba8b1f876ec8e278c60d63ed029737e439687e1e
krawnick <kraw.nick@gmail.com>
2023-09-09 16:55:03 +0500

    lesson67 (test)

---
+++ backup/test66/index.ts
@@ -0,0 +1,58 @@
+class Box {
+  height: number
+  width: number
+  volume: number | undefined
+  _content: string | undefined
+
+  constructor(width: number, volume?: number, content?: string) {
+    this.height = 500
+    this.width = width
+    this.volume = volume
+    this._content = content
+  }
+
+  calculateVolume(): void {
+    if (!this.volume) {
+      this.volume = this.width * this.height
+      console.log(`Volume package: ${this.volume}`)
+    } else {
+      console.log(`Volume package: ${this.volume}`)
+    }
+  }
+
+  checkBoxSize(transport: number): string
+  checkBoxSize(transport: number[]): string
+  checkBoxSize(transport: number | number[]): string {
+    if (typeof transport === 'number') {
+      return transport >= this.width ? 'OK' : 'Not OK'
+    } else if (Array.isArray(transport)) {
+      return transport.some((t) => t >= this.width) ? 'OK' : 'Not OK'
+    } else {
+      return 'Error type'
+    }
+  }
+
+  // get content() {
+  // 	return this._content
+  // }
+
+  // set content(value) {
+  // 	this._content = `Date: ${new Date().toTimeString()}, Content: ${value}`
+  // }
+
+  async content(value: string) {
+    const date = await new Date().toTimeString()
+    this._content = `Date: ${date}, Content: ${value}`
+  }
+}
+
+const firstBox = new Box(250)
+firstBox.volume = 50000
+
+console.log(firstBox.calculateVolume())
+
+console.log(firstBox.checkBoxSize([240, 230, 220]))
+console.log(firstBox.checkBoxSize(270))
+
+// console.log(firstBox.content = 'test')
+// console.log(firstBox.content)
