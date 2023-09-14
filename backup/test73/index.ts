e16703b6e559733e0cae7d683295e3b051894a03
krawnick <kraw.nick@gmail.com>
2023-09-14 21:35:21 +0500

    lesson74 (test)

--- backup/test73/index.ts
+++ backup/test73/index.ts
@@ -10,10 +10,6 @@ class Player {
   public server: string // public по умолчанию
   protected consent: boolean
 
-  constructor(login: string) {
-    this.#login = login
-  }
-
   static {
     Player.game = setName()
   }
@@ -37,50 +33,18 @@ class Player {
   static getGameName() {
     return Player.game
   }
-
-  logIn = () => {
-    return `Player ${this.#login} online`
-  }
-
-  connect() {
-    // Do smth
-    return this
-  }
-
-  isPro(): this is CompetitivePlayer {
-    return this instanceof CompetitivePlayer
-  }
 }
 
 class CompetitivePlayer extends Player {
   rank: number
-
-  checkLogin() {
-    return this.logIn()
-  }
 
   private isConsented() {
     this.consent ? 'Yes' : 'No'
   }
 }
 
-const player = new Player('test')
-console.log(player.logIn())
-console.log(player.connect().logIn())
-
-const player2 = new CompetitivePlayer('test2')
-console.log(player2.checkLogin())
-console.log(player2.connect().logIn())
-
-const somePlayer: Player | CompetitivePlayer = new CompetitivePlayer('test3')
-somePlayer.isPro() ? console.log(somePlayer) : console.log(somePlayer)
-
-// const test = player.logIn.bind(player)
-const test = player.logIn
-test()
-
-// new Player()
-// console.log(Player.getGameName())
+new Player()
+console.log(Player.getGameName())
 
 class User {
   public email: string
