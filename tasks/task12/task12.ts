a0274fa94980e64e2a52847b9a108999798da78e
krawnick <kraw.nick@gmail.com>
2023-09-19 19:19:00 +0500

    ready task12 (isInt, isMin)

--- tasks/task12/task12.ts
+++ tasks/task12/task12.ts
@@ -20,8 +20,8 @@ class ShippingContainer implements ICuboid {
   // @Min(1)
   // @Max(8)
   height: number
-
-  createdAt: Date
+  createdAt: any
+  lastCalc: any
 
   constructor(width: number, lenght: number, height: number) {
     this.width = width
@@ -29,23 +29,90 @@ class ShippingContainer implements ICuboid {
     this.height = height
   }
 
+  @fixLastCalc('calcArea')
   calcArea(multiply?: number): number {
     return this.width * this.length * (multiply ? multiply : 1)
   }
 
+  @fixLastCalc('calcVolume')
   calcVolume(multiply?: number): number {
     return this.width * this.length * this.height * (multiply ? multiply : 1)
   }
 }
 
 const container = new ShippingContainer(10, 100, 10)
+container.width = 1
+container.height = 5
 console.log(container)
-// container.width = 0
-// container.height = 5
-// console.log(container.calcVolume())
+console.log(container.calcArea())
+console.log(container.lastCalc)
+console.log(container.calcVolume())
+console.log(container.lastCalc)
+
+console.log(container.calcVolume())
 
 function createdAt<T extends { new (...args: any[]): {} }>(constructor: T) {
   return class extends constructor {
     createdAt = new Date()
+  }
+}
+
+function fixLastCalc(method: string) {
+  // console.log('method', method)
+  return (
+    target: Object,
+    propertyKey: string | symbol,
+    descriptor: PropertyDescriptor
+  ): PropertyDescriptor | void => {
+    // console.log('target', target)
+    // console.log('PK', propertyKey)
+    // console.log('descriptor', descriptor)
+    console.log('descriptor.value', descriptor.value)
+
+    const oldValue = descriptor.value
+    descriptor.value = function (this: any, ...args: any[]) {
+      this.lastCalc = `Последний подсчет ${method} был ${new Date()}`
+      return oldValue.apply(this, args)
+    }
+  }
+}
+
+function isInt() {
+  return function (target: Object, propertyKey: string | symbol) {
+    let symbol = Symbol()
+
+    const getter = function (this: any) {
+      return this[symbol]
+    }
+
+    const setter = function (this: any, newAmount: unknown) {
+      if (typeof newAmount === 'number' && Number.isInteger(newAmount)) {
+        this[symbol] = newAmount
+      } else {
+        throw new Error('Получено не целое число')
+      }
+    }
+
+    Object.defineProperty(target, propertyKey, { get: getter, set: setter })
+  }
+}
+
+function isMin(value: number) {
+  return function (target: Object, propertyKey: string | symbol) {
+    let symbol = Symbol()
+
+    const getter = function (this: any) {
+      return this[symbol]
+    }
+
+    const setter = function (this: any, newAmount: number) {
+      if (newAmount > value) {
+        this[symbol] = newAmount
+      } else {
+        throw new Error('Получено значение меньше минимального')
+      }
+    }
+
+    Object.defineProperty(target, propertyKey, { get: getter, set: setter })
   }
 }
