1c0f68f6673c7475b7cb5ba55148e578bf741dbc
krawnick <kraw.nick@gmail.com>
2023-09-20 19:26:27 +0500

    ready task12 (func validate (not isMax))

--- tasks/task12/task12.ts
+++ tasks/task12/task12.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata'
 interface ICuboid {
   width: number
   length: number
@@ -8,17 +9,17 @@ interface ICuboid {
 
 @createdAt
 class ShippingContainer implements ICuboid {
-  // @IsInt()
-  // @Min(1)
+  @isInt()
+  @isMin(1)
   width: number
 
-  // @IsInt()
-  // @Min(1)
+  @isInt()
+  @isMin(1)
   length: number
 
-  // @IsInt()
-  // @Min(1)
-  // @Max(8)
+  @isInt()
+  @isMin(1)
+  @isMax(8)
   height: number
   createdAt: any
   lastCalc: any
@@ -27,6 +28,7 @@ class ShippingContainer implements ICuboid {
     this.width = width
     this.length = lenght
     this.height = height
+    validate(this, 'width', width)
   }
 
   @fixLastCalc('calcArea')
@@ -41,7 +43,7 @@ class ShippingContainer implements ICuboid {
 }
 
 const container = new ShippingContainer(10, 100, 10)
-container.width = 1
+container.width = 0
 container.height = 5
 console.log(container)
 console.log(container.calcArea())
@@ -79,40 +81,87 @@ function fixLastCalc(method: string) {
 
 function isInt() {
   return function (target: Object, propertyKey: string | symbol) {
-    let symbol = Symbol()
+    // let symbol = Symbol()
 
-    const getter = function (this: any) {
-      return this[symbol]
-    }
+    // const getter = function (this: any) {
+    //     return this[symbol]
+    // }
 
-    const setter = function (this: any, newAmount: unknown) {
-      if (typeof newAmount === 'number' && Number.isInteger(newAmount)) {
-        this[symbol] = newAmount
-      } else {
-        throw new Error('Получено не целое число')
-      }
-    }
+    // const setter = function (this: any, newAmount: unknown) {
+    //     if (typeof newAmount === 'number' && Number.isInteger(newAmount)) {
+    //         this[symbol] = newAmount
+    //     } else {
+    //         throw new Error('Получено не целое число')
+    //     }
+    // }
 
-    Object.defineProperty(target, propertyKey, { get: getter, set: setter })
+    // Object.defineProperty(target, propertyKey, { get: getter, set: setter, enumerable: true, configurable: true })
+    Reflect.defineMetadata('isInt', true, target, propertyKey)
   }
 }
 
 function isMin(value: number) {
   return function (target: Object, propertyKey: string | symbol) {
-    let symbol = Symbol()
+    // let symbol = Symbol()
 
-    const getter = function (this: any) {
-      return this[symbol]
-    }
+    // const getter = function (this: any) {
+    //     return this[symbol]
+    // }
 
-    const setter = function (this: any, newAmount: number) {
-      if (newAmount > value) {
-        this[symbol] = newAmount
-      } else {
-        throw new Error('Получено значение меньше минимального')
-      }
-    }
+    // const setter = function (this: any, newAmount: number) {
+    //     if (newAmount > value) {
+    //         this[symbol] = newAmount
+    //     } else {
+    //         throw new Error('Получено значение меньше минимального')
+    //     }
+    // }
 
-    Object.defineProperty(target, propertyKey, { get: getter, set: setter })
+    // Object.defineProperty(target, propertyKey, { get: getter, set: setter, enumerable: true, configurable: true })
+    Reflect.defineMetadata('isMin', value, target, propertyKey)
   }
+}
+
+function isMax(value: number) {
+  return function (target: Object, propertyKey: string | symbol) {
+    // let symbol = Symbol()
+
+    // const getter = function (this: any) {
+    //     return this[symbol]
+    // }
+
+    // const setter = function (this: any, newAmount: number) {
+    //     if (newAmount > value) {
+    //         this[symbol] = newAmount
+    //     } else {
+    //         throw new Error('Получено значение больше максимального')
+    //     }
+    // }
+
+    // Object.defineProperty(target, propertyKey, { get: getter, set: setter, enumerable: true, configurable: true })
+    Reflect.defineMetadata('isMax', value, target, propertyKey)
+  }
+}
+
+function validate(target: any, propertyKey: string, value: any): boolean {
+  if (
+    Reflect.getMetadata('isInt', target, propertyKey) &&
+    (!Number.isInteger(value) || value !== parseInt(value))
+  ) {
+    throw new Error(`${propertyKey} - нецелое число`)
+  }
+
+  if (
+    Reflect.hasMetadata('isMin', target, propertyKey) &&
+    value < Reflect.getMetadata('isMin', target, propertyKey)
+  ) {
+    throw new Error(
+      `Минимальное значение ${propertyKey} должно быть ${Reflect.getMetadata(
+        'isMin',
+        target,
+        propertyKey
+      )}`
+    )
+  }
+
+  return true
 }
