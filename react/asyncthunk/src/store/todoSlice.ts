459023952abc6531d766bd76106cb6b0d6451b48
krawnick <kraw.nick@gmail.com>
2023-10-06 18:35:15 +0500

    asyncthunk

--- react/asyncthunk/src/store/todoSlice.ts
+++ react/asyncthunk/src/store/todoSlice.ts
@@ -57,6 +57,35 @@ export const addNewTodo = createAsyncThunk<
   return data
 })
 
+// export const toggleStatus = createAsyncThunk<Todo, string, {rejectWithValue: string, state: {todos: TodosState}}>(
+export const toggleStatus = createAsyncThunk<
+  Todo,
+  string,
+  { rejectWithValue: string; state: { todos: TodosState } }
+>('todos/toggleStatus', async function (id, { rejectWithValue, getState }) {
+  const todo = getState().todos.list.find((todo) => todo.id === id)
+
+  if (todo) {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/todos/${id}`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          completed: !todo.completed,
+        }),
+      }
+    )
+
+    if (!response.ok) {
+      return rejectWithValue("Can't toggle status. Server error.")
+    }
+  }
+  return rejectWithValue("Can't toggle status. Server error.")
+})
+
 const initialState: TodosState = {
   list: [],
   loading: false,
